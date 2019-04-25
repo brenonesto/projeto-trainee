820bf55a2570d0cc22c9d55cd8881a24506843e6
brenonesto <breno.nesto@hotmail.com>
2019-04-25 17:47:50 -0300

    sections

--- js/home.js
+++ js/home.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  $('.carro1').slick({
+    
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
