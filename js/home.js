968565634dc22bd427c688cc9a68f759556ba5ec
brenonesto <breno.nesto@hotmail.com>
2019-04-26 21:00:28 -0300

    newsletter

--- js/home.js
+++ js/home.js
@@ -1,28 +1,12 @@
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
 $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-  $('.carro1').slick({
-    
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+  $('.parallax').parallax();
+});
+      
