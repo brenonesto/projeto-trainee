8b6e158498ff052d901f8e75196ff3c831a2087b
brenonesto <breno.nesto@hotmail.com>
2019-04-29 23:05:15 -0300

    minha parte

--- js/home.js
+++ js/home.js
@@ -9,4 +9,58 @@ $(document).ready(function () {
 $(document).ready(function(){
   $('.parallax').parallax();
 });
-      
+
+var $root = $('html, body');
+
+$('#quem').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
+
+$('#ejs1').click(function () {
+  $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+
+  return false;
+});
+
+$('#parceiros1').click(function () {
+  $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+
+  return false;
+});
+
+$('#fale1').click(function () {
+  $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+
+  return false;
+});
+
+$('#home1').click(function () {
+  $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+
+  return false;
+});
+
+$('#home2').click(function () {
+  $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+
+  return false;
+});
+
+$("form").submit(function(e){
+  e.preventDefault();
+  alert("Mensagem Enviada!");
+});
